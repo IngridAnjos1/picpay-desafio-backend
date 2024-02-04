cc8c0167ef7d659525d1335d374f254732d025e4
Ingrid Anjos <ingrid_anjos@hotmail.com>
2024-02-04 08:47:25 -0300

    feat: ajuste de identacao UserController

--- src/main/java/com/picpaysimplificado/controller/UserController.java
+++ src/main/java/com/picpaysimplificado/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+
 import java.util.List;
 
 @RestController
@@ -22,7 +23,6 @@ public class UserController {
         User newUser = userService.createUser(user);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
-
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers() {
         List<User> users = this.userService.getAllUsers();
